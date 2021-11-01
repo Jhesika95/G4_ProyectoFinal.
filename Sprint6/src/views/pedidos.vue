754bdd644bee7e46c5d8629f9f48f70ebde4f027
Jhesika95 <90456454+Jhesika95@users.noreply.github.com>
2021-10-31 21:37:05 -0500

    Add files via upload

---
+++ Sprint6/src/views/pedidos.vue
@@ -0,0 +1,5 @@
+<template>
+<li class="nav-item">
+    <a class="nav-link" href="#"><h5>Mis pedidos</h5></a>
+</li>  
+</template>
---
+++ Sprint6/src/views/salir.vue
@@ -0,0 +1,4 @@
+<template>
+    <a class="btn btn-light ms-lg-5" href="1.login.html" onclick="Deseasalir()" type="button"><h5>Salir</h5></a>
+  
+</template>
